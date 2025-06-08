c3d30ba2a2a4791648b2bba172b24d2ecf2640b9
guilhermesoi <guilhermeidhabbo@gmail.com>
2025-06-08 11:30:13 -0300

    Update server.js

--- server.js
+++ server.js
@@ -3,13 +3,13 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const fetch = require('node-fetch');
-const cors = require('cors');
+
 require('dotenv').config();
 
 const app = express();
 app.use(express.static('.'));
 app.use(bodyParser.json());
-app.use(cors());
+
 
 app.post('/chat', async (req, res) => {
   const userMessage = req.body.message;
